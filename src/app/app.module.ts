2f181a478ea0c0ca12af5497ba74e21bb12109cc
LeonardoAvendaño <leavendanoro@unal.edu.co>
2018-03-04 16:50:02 -0500

    Se añaden servicios para la lectura de datos y se añade un componente para la visualización de mensajes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,19 +6,27 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
 import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+import { MessageService } from './message.service';
+import { MessagesComponent } from './messages/messages.component';
+import { HeroService } from './hero.service';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HeroesComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    MessagesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+      HeroService,
+      MessageService,
+    /* . . . */
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
